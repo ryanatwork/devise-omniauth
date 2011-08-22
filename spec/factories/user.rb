36ac030419d74a22d0fdf197aad5740e7183944b
Ryan Resella <ryan@codeforamerica.org>
2011-08-21 23:37:41 -0700

    Added factory for user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) {|n| "email#{n}@testcompany.com"}
+    email "sample@example.com"
     password "123456"
     password_confirmation "123456"
   end
