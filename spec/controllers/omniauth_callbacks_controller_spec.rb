22250d05c66719956ea5b326c0fbf977a46f0d6a
Ryan Resella <ryan@codeforamerica.org>
2011-08-21 23:38:34 -0700

    Updated specs to test for new user or existing user

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -8,10 +8,25 @@ describe Users::OmniauthCallbacksController do
       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
     end
 
-    it "should create and in a new user" do
+    it "should create and sign in a new user" do
       get :facebook
-      response.should redirect_to new_user_registration_url
+      @user = User.last
+      @user.email.should == "sample@example.com"
+      response.should redirect_to root_path
+      subject.current_user.should_not be_nil
     end
   end
 
+  describe "#facebook" do
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+      @user = Factory(:user)
+    end
+
+    it "should sign in an existing user" do
+      get :facebook
+      subject.current_user.should_not be_nil
+    end
+  end
 end
