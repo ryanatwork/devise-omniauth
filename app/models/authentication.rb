d5629c1849499b0dea4fc593518508fa7e2b5bb7
Ryan Resella <ryan@codeforamerica.org>
2011-08-22 21:36:31 -0700

    Added relationship between models

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,6 @@
 class Authentication < ActiveRecord::Base
   attr_accessible :user_id, :provider, :uid, :access_token, :access_secret
+
+  has_one :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
+  has_many :authentications
+
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     data = access_token['extra']['user_hash']
     if user = User.find_by_email(data["email"])
